9a78ab363e883b7770ea578a8f826be70ea0611b
Everett Williams <ebw13@students.uwf.edu>
2025-04-03 15:31:14 -0500

    formated prompt

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ def main():
         starter_word = input("Please enter your chosen starter word: ")
 
         length = int(input("Please enter length of sentence: "))
-        #insert checks for legal input
 
         generated_sentance = generator.generate(starter_word, length)
         print(generated_sentance)
 
-        to_exit = input("Type exit to leave, any other key to try enter another word.")
+        to_exit = input("Type exit to leave, any other key to try enter another word: ")
         if to_exit == "Exit" or to_exit == "exit":
             break
         
