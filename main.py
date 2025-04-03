749e4b173b8caab90aeb6c578d7d6c39963318d8
Everett Williams <ebw13@students.uwf.edu>
2025-04-02 19:25:52 -0500

    added text generation code, some errors exist

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import text_generator as tg
 def main():
     generator = tg.TextGenerator()
 
-    #loop location
-        #get starter text and sentance length(have validation for legal values)
-        #call generator.text(starter_text, length)
-        #ask if they would like to continue
+    while True:
+        starter_word = input("Please enter your chosen starter word.")
+
+        length = input("Please enter length of sentence.")
+        #insert checks for legal input
+
+        generated_sentance = generator.generate(starter_word, length)
+        print(generated_sentance)
+
+        to_exit = input("Type exit to leave, any other key to try enter another word.")
+        if to_exit == "Exit" or to_exit == "exit":
+            break
+        
 
 if __name__ == "__main__":
-        main()
+    main()
--- text_generator.py
+++ text_generator.py
@@ -1,5 +1,35 @@
 import pandas as pd
+from collections import defaultdict
+import random
 
 class TextGenerator:
     def __init__(self):
         self.corpus = pd.read_pickle("corpus.pkl")
+        self.markov_chain = self._create_chain(self.corpus)
+
+    #creates the markov chain dictionary
+    def _create_chain(self, text):
+        words = text.split(' ')
+
+        dictionary = defaultdict(list)
+
+        for current_word, next_word in zip(words[0:-1], words[1:]):
+            dictionary[current_word].append(next_word)
+
+        dictionary = dict(dictionary)
+
+        return dictionary
+        
+    #generates sentance based off starter word and length
+    def generate(self, starter_word, length):
+        word1 = starter_word
+        sentence = word1.capitalize()
+
+        for i in range(length - 1):
+            word2 = random.choice(self.markov_chain[word1])
+            word1 = word2
+            sentence += ' ' + word2
+
+    
+        sentence += '.'
+        return sentence
