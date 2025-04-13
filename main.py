e0df4697172d268555e154b920f1ebe36b007ae3
RyanBlankenbeker <156377624+RyanBlankenbeker@users.noreply.github.com>
2025-04-12 23:55:56 -0400

    added comments to the code

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import text_generator as tg
 
 def main():
-    generator = tg.TextGenerator()
+    generator = tg.TextGenerator() # Creates instance of TextGenerator class
 
     while True:
-        starter_word = input("Please enter your chosen starter word: ")
+        starter_word = input("Please enter your chosen starter word: ") 
 
         length = int(input("Please enter length of sentence: "))
 
-        generated_sentence = generator.generate(starter_word, length)
+        generated_sentence = generator.generate(starter_word, length)  # Generates the sentence that is output to user
         print("\nGenerated Sentence: ")
         print(generated_sentence)
 
-        to_exit = input("\nType exit to leave, any other key to try enter another word: ")
-        if to_exit == "Exit" or to_exit == "exit":
+        to_exit = input("\nType exit to leave, any other key to try enter another word: ")  
+        if to_exit == "Exit" or to_exit == "exit":  # Exits program whenever user types "exit"
             break
         
 
--- text_generator.py
+++ text_generator.py
@@ -17,15 +17,15 @@ class TextGenerator:
             print("Downloading Brown corpus...")
             nltk.download('brown', download_dir='nltk_data')
 
-        if not os.path.exists("corpus.pkl"):
+        if not os.path.exists("corpus.pkl"):  # if pkl file does not exist, creates one
             self._generate_corpus_pkl()
 
         with open("corpus.pkl", "rb") as f:
             self.corpus = pickle.load(f)
 
-        self.markov_chain = self._create_chain(self.corpus)
+        self.markov_chain = self._create_chain(self.corpus) # Builds the Markov chain fom the Brown corpus
 
-    def _generate_corpus_pkl(self):
+    def _generate_corpus_pkl(self):   # Converts corpus into all lowercase and then into a pkl file
         print("Generating corpus.pkl from Brown corpus...")
         words = brown.words()
         words = [word.lower() for word in words if word.isalpha()]
@@ -33,21 +33,21 @@ class TextGenerator:
             pickle.dump(words, f)
         print("corpus.pkl generated!")
 
-    def _create_chain(self, text):
+    def _create_chain(self, text):   # Builds a Markov chain as a dictionary where each word maps to next possible word
         dictionary = defaultdict(list)
         for current_word, next_word in zip(text[:-1], text[1:]):
             dictionary[current_word].append(next_word)
         return dict(dictionary)
 
-    def generate(self, starter_word, length):
+    def generate(self, starter_word, length): # This method generates the sentence that is output to the user
         word1 = starter_word.lower()
 
         if word1 not in self.markov_chain:
-            return f"Sorry, '{starter_word}' is not in the corpus. Try another word."
+            return f"Sorry, '{starter_word}' is not in the corpus. Try another word."   # If user inputs word not in the corpus
 
-        sentence = word1.capitalize()
+        sentence = word1.capitalize()  # Begins generated sentence with capital letter
 
-        for i in range(length - 1):
+        for i in range(length - 1):   # Generates remaining words
             next_words = self.markov_chain.get(word1)
             if not next_words:
                 break
